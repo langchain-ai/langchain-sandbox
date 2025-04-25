dd5ca5b713b1d1849b291488a977446027b37c9c
vbarda <vadym@langchain.dev>
2025-04-25 11:44:25 -0400

    allow returning session bytes & metadata directly

--- libs/sandbox-py/langchain_sandbox/tool.py
+++ libs/sandbox-py/langchain_sandbox/tool.py
@@ -1,9 +1,12 @@
 """LangChain tool for running python code in a PyodideSandbox."""
 
+from typing import Annotated, Any, Literal
+
 from langchain_core.callbacks import (
     AsyncCallbackManagerForToolRun,
     CallbackManagerForToolRun,
 )
+from langchain_core.messages import ToolMessage
 from langchain_core.runnables import RunnableConfig
 from langchain_core.tools import BaseTool
 from pydantic import BaseModel, Field
@@ -21,10 +24,35 @@ def _get_default_pyodide_sandbox() -> PyodideSandbox:
     )
 
 
-class PythonInputs(BaseModel):
-    """Python code to execute in the sandbox."""
+def _get_last_tool_artifact(
+    state: dict[str, Any],
+) -> dict[str, bytes | dict | None] | None:
+    if "messages" not in state:
+        error_msg = "Expected state to have 'messages' key."
+        raise ValueError(error_msg)
 
-    code: str = Field(description="Code to execute.")
+    last_tool_message = next(
+        (
+            msg
+            for msg in reversed(state["messages"])
+            if isinstance(msg, ToolMessage) and msg.name == "python_code_sandbox"
+        ),
+        None,
+    )
+    if last_tool_message is None:
+        return None
+
+    if (
+        "session_bytes" not in last_tool_message.artifact
+        or "session_metadata" not in last_tool_message.artifact
+    ):
+        error_msg = (
+            "Expected tool artifact to have 'session_bytes' and "
+            "'session_metadata' keys."
+        )
+        raise ValueError(error_msg)
+
+    return last_tool_message.artifact
 
 
 class PyodideSandboxTool(BaseTool):
@@ -77,14 +105,44 @@ class PyodideSandboxTool(BaseTool):
         "- If you need to make web requests, use `httpx.AsyncClient`."
     )
     sandbox: PyodideSandbox = Field(default_factory=_get_default_pyodide_sandbox)
-    args_schema: type[BaseModel] = PythonInputs
+    use_checkpointer: bool = False
+    response_format: Literal["content_and_artifact"] = "content_and_artifact"
+
+    def __init__(self, **kwargs: dict[str, Any]) -> None:
+        """Initialize the tool with correct args schema."""
+        super().__init__(**kwargs)
+        if self.use_checkpointer:
+            try:
+                from langgraph.prebuilt import InjectedState
+            except ImportError as e:
+                error_msg = (
+                    "The 'langgraph' package is required when use_checkpointer=True. "
+                    "Please install it with 'pip install langgraph'."
+                )
+                raise ImportError(error_msg) from e
+
+            class PythonInputs(BaseModel):
+                """Python code to execute in the sandbox."""
+
+                state: Annotated[dict, InjectedState]
+                code: str = Field(description="Code to execute.")
+
+        else:
+
+            class PythonInputs(BaseModel):
+                """Python code to execute in the sandbox."""
+
+                code: str = Field(description="Code to execute.")
+
+        self.args_schema: type[BaseModel] = PythonInputs
 
     def _run(
         self,
         code: str,
+        state: dict,
         config: RunnableConfig,
         run_manager: CallbackManagerForToolRun | None = None,
-    ) -> str:
+    ) -> tuple[str, dict[str, bytes | dict | None]]:
         """Use the tool."""
         error_msg = (
             "Sync invocation of PyodideSandboxTool is not supported - "
@@ -95,12 +153,36 @@ class PyodideSandboxTool(BaseTool):
     async def _arun(
         self,
         code: str,
+        state: dict,
         config: RunnableConfig,
         run_manager: AsyncCallbackManagerForToolRun | None = None,
-    ) -> str:
+    ) -> tuple[str, dict[str, bytes | dict | None]]:
         """Use the tool asynchronously."""
-        session_id = config.get("configurable", {}).get("thread_id")
-        result = await self.sandbox.execute(code, session_id=session_id)
+        if self.use_checkpointer:
+            # If we're using a checkpointer, load the most recent available
+            # tool artifact for this tool from the checkpointed messages.
+            # The tool artifact will contain the session bytes and session metadata,
+            # which allows us to resume code execution from the same sandbox session.
+            last_artifact = _get_last_tool_artifact(state)
+            sandbox_kwargs = {}
+            if last_artifact is not None:
+                sandbox_kwargs = {
+                    "session_bytes": last_artifact["session_bytes"],
+                    "session_metadata": last_artifact["session_metadata"],
+                }
+
+            result = await self.sandbox.execute(code, **sandbox_kwargs)
+        else:
+            # If we're not using a checkpointer, we rely
+            # on passing the thread_id as a session_id
+            # and storing session bytes and metadata on disk.
+            session_id = config.get("configurable", {}).get("thread_id")
+            result = await self.sandbox.execute(code, session_id=session_id)
+
+        artifact = {
+            "session_bytes": result.session_bytes,
+            "session_metadata": result.session_metadata,
+        }
         if result.stderr:
-            return f"Error during execution: {result.stderr}"
-        return result.stdout
+            return f"Error during execution: {result.stderr}", artifact
+        return result.stdout, artifact
